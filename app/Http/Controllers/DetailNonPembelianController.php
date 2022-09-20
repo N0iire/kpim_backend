94535d4a16993bdac840d54cceda12c76fab82df
N0iire <hirukorei@gmail.com>
2022-09-20 13:18:08 +0700

    Starter Pack Detail Non Pembelian

---
+++ app/Http/Controllers/DetailNonPembelianController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetailNonPembelian;
+use App\Http\Requests\StoreDetailNonPembelianRequest;
+use App\Http\Requests\UpdateDetailNonPembelianRequest;
+
+class DetailNonPembelianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDetailNonPembelianRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDetailNonPembelianRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DetailNonPembelian  $detailNonPembelian
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetailNonPembelian $detailNonPembelian)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DetailNonPembelian  $detailNonPembelian
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetailNonPembelian $detailNonPembelian)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDetailNonPembelianRequest  $request
+     * @param  \App\Models\DetailNonPembelian  $detailNonPembelian
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDetailNonPembelianRequest $request, DetailNonPembelian $detailNonPembelian)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DetailNonPembelian  $detailNonPembelian
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetailNonPembelian $detailNonPembelian)
+    {
+        //
+    }
+}
